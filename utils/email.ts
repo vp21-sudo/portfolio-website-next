3b42d679d704ef9c4ae34c366facc5e3ac6de54a
vp21-sudo <vishwaprasad11@gmail.com>
2024-04-07 01:40:24 +0530

    type error fix in 'utils/email.ts'

--- utils/email.ts
+++ utils/email.ts
@@ -12,7 +12,7 @@ const sendEmail = async ({ to, subject, text, html, attachments }: nodemailerInp
     try{
         const transporter = nodemailer.createTransport({
             host: process.env.MAIL_HOST,
-            port: process.env.MAIL_PORT,
+            port: Number(process.env.MAIL_PORT),
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASSORD
